e404f34324944b554f2dca8112413ca6a08ab1b6
Lucas Viana Torres <lucas.lvtorres@gmail.com>
2025-02-19 15:17:14 -0300

    PUT - Adicionando font poppins

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from "../../components/banner/banner.component";
+import { ProgramasComponent } from "../../components/programas/programas.component";
 
 @Component({
   selector: 'app-home',
-  imports: [BannerComponent],
+  imports: [BannerComponent, ProgramasComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   standalone: true
