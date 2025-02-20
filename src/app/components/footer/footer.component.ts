80d57ccce3b246ec49b154f0435c2b8ce58a4500
Lucas Viana Torres <lucas.lvtorres@gmail.com>
2025-02-20 16:13:48 -0300

    PUT - Finalizando component footer.

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface LinkUtil {
+  texto: string;
+  url: string;
+}
+
+interface RedeSocial {
+  nome: string;
+  url: string;
+  icone: string;
+}
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
+  currentYear: number = new Date().getFullYear();
 
+  linksUteis: LinkUtil[] = [
+    { texto: 'Planos', url: '/planos' },
+    { texto: 'Programas', url: '/programas' },
+    { texto: 'Unidades', url: '/academias' },
+    { texto: 'FAQ', url: '/faq' },
+  ];
+
+  redesSociais: RedeSocial[] = [
+    { nome: 'Facebook', url: 'https://facebook.com', icone: 'fab fa-facebook-f' },
+    { nome: 'Instagram', url: 'https://instagram.com', icone: 'fab fa-instagram' },
+    { nome: 'Twitter', url: 'https://twitter.com', icone: 'fab fa-twitter' },
+    { nome: 'YouTube', url: 'https://youtube.com', icone: 'fab fa-youtube' },
+  ];
+
+  contato = {
+    email: 'contato@academiafit.com',
+    telefone: '(11) 98765-4321',
+    endereco: 'Rua do Fitness, 123 - São Paulo, SP',
+  };
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,10 +4,11 @@ import { ProgramasComponent } from "../../components/programas/programas.compone
 import { AcademiasComponent } from "../../components/academias/academias.component";
 import { PlanosAcademiaComponent } from "../../components/planos-academia/planos-academia.component";
 import { DepoimentosComponent } from "../../components/depoimentos/depoimentos.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-home',
-  imports: [BannerComponent, ProgramasComponent, AcademiasComponent, PlanosAcademiaComponent, DepoimentosComponent],
+  imports: [BannerComponent, ProgramasComponent, AcademiasComponent, PlanosAcademiaComponent, DepoimentosComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   standalone: true
