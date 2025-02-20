53996a4288b0c28789a0acb10bf57071d15e1589
Lucas Viana Torres <lucas.lvtorres@gmail.com>
2025-02-20 14:37:32 -0300

    PUT - Finalizando componente academias

--- src/app/components/academias/academias.component.ts
+++ src/app/components/academias/academias.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-academias',
-  imports: [],
+  imports: [
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './academias.component.html',
   styleUrl: './academias.component.scss'
 })
